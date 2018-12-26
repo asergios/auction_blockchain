864e7f06e6df5fe38cbc444530d78d7ccf111bd6
asergio <asergio@ua.pt>
2018-12-26 16:46:14 +0000

    Changes Logic of Receipt Manager > Adds diferent folder for diferent users on a way that multiple users can use the client > Adds .pwd file that is used to create the key used to encrypt the receipts

--- src/client/receiptmanager/receiptmanager.py
+++ src/client/receiptmanager/receiptmanager.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 from ...common.cryptmanager import *
 from ..util import *
+from ..cartaodecidadao import CartaoDeCidadao
 from Crypto.Hash import SHA256
 from hmac import compare_digest
 import hashlib
@@ -10,79 +11,105 @@ import sys
 
 class ReceiptManager:
 
-	def save_receipt(self, auction_id, bid_id, receipt, salt):
-		'''
-			Save Encrypted Receipt
-		'''
-		self.has_writting_perm()
-		file = open('src/client/receiptmanager/receipts/'+auction_id+"_"+bid_id, 'wb')
+	def __init__(self, cc):
+		self.cc = cc
+		self.cc_number = self.cc.get_identity()[1]
 
-		pw = self.get_password(salt)
+	def save_receipt(self, auction_id, receipt):
+		'''
+			Save Receipt
+		'''
+		# Checking for Permissions on Folder
+		self.check_perm()
+		# Opening File Where Receipt Will Be Stored
+		file = open('src/client/receiptmanager/receipts/'+self.cc_number+'/'+auction_id, 'wb')
+		# Getting User Password Key
+		pw = self.get_key()
+		# Building HMAC for receipt
 		hmac = SHA256.new(receipt)
 		hmac = hmac.digest()
-
+		# Encrypting receipt with key
 		result = encrypt(pw, (hmac+receipt))
+		# Writting on File
 		file.write(result)
+		file.close()
 
-	def get_receipt(self, auction_id, bid_id, salt):
+	def get_receipt(self, auction_id):
 		'''
-			Get Decrypted Receipt
+			Get Receipt
 		'''
-		self.has_reading_perm()
-		file = open('src/client/receiptmanager/receipts/'+auction_id+"_"+bid_id, 'rb')
+		# Checking for Permissions on Folder
+		self.check_perm()
+		# Checking if such receipt exists
+		if os.path.isfile('src/client/receiptmanager/receipts/'+self.cc_number+'/'+auction_id):
+			# Opening receipt file
+			file = open('src/client/receiptmanager/receipts/'+self.cc_number+'/'+auction_id, 'rb')
+			# Getting the key
+			pw = self.get_key()
+			# Decrypting Receipt
+			result = decrypt(pw, file.read())
+			file.close()
 
-		pw = getpass.getpass("Password: ", sys.stdout)
-		pw = self.password_builder(pw, salt)
-		result = decrypt(pw, file.read())
-		if compare_digest(result[:32], SHA256.new(result[32:]).digest() ):
-			return result[32:]
-		return False
+			# Checking integrity of the receipt
+			if(compare_digest(result[:32], SHA256.new(result[32:]).digest())):
+				return result[32:]
+			else:
+				print( colorize("ERROR: Corrupted File Or Unauthorized Access", 'red') )
+				input("Press any key to continue...")
+				return None
+		else:
+			print( colorize("ERROR: Receipt Not Found", 'red') )
+			input("Press any key to continue...")
+			return None
 
-	def get_password(self, salt):
+	def get_key(self):
 		'''
 			Getting new password from user
 		'''
-		# Getting Password From User
-		while True:
-			pw = getpass.getpass("Please pick a password (minimum 6 character): ", sys.stdout)
-			if(len(pw) >= 6):
-				clean(True)
-				clean(lines=1)
-				repeat = getpass.getpass("Repeat Password: ", sys.stdout)
-				if(pw == repeat):
-					clean(True)
-					break
-				else:
-					print( colorize('Passwords must match!', 'red') )
-					clean()
-			else:
-				print( colorize('Password must be at least 6 characters!', 'red') )
-				clean()
+		# Checking existence of user dir
+		self.check_dir()
 
-		return self.password_builder(pw,salt)
+		# Checking if there is a password already set
+		if os.path.isfile("src/client/receiptmanager/receipts/"+self.cc_number+"/.pwd"):
+			# Getting .pwd contents and sign it
+			file = open("src/client/receiptmanager/receipts/"+self.cc_number+"/.pwd", "rb")
+			key = self.cc.sign(file.read())
+			file.close()
+		else:
+			# Building new random for password
+			new = os.urandom(128)
+			file = open("src/client/receiptmanager/receipts/"+self.cc_number+"/.pwd", "wb")
+			file.write(new)
+			file.close()
+			key = self.cc.sign(new)
 
+		# Return Hashing Of Password
+		return self.password_builder(key, self.cc.get_public_key()[10:26])
 
 	def password_builder(self, password, salt):
 		'''
 			Hashing of Password
 		'''
-		password_hash = hashlib.pbkdf2_hmac('sha256', bytearray(password,'UTF-8'), b'\x00', 1000, 16)
+		password_hash = hashlib.pbkdf2_hmac('sha256', password, salt, 1000, 16)
 		return password_hash
 
-	def has_reading_perm(self):
+	def check_dir(self):
 		'''
-			Checks read permissions
+			Check if DIR exists, if it doesn't, create a new one
+		'''
+		if os.path.isdir("src/client/receiptmanager/receipts/"+self.cc_number): return
+		else: os.mkdir("src/client/receiptmanager/receipts/"+self.cc_number)
+
+	def check_perm(self):
+		'''
+			Checks read and write permissions
 		'''
 		while(not os.access('src/client/receiptmanager/receipts', os.R_OK)):
-			print( colorize("I have no permissions to read your receipt, please give read permissions at src/client/receiptmanager/receipts", 'red') )
+			print( colorize("I have no READ permissions, please allow READ permissions at src/client/receiptmanager/receipts", 'red') )
 			input("Press any key to try again...")
 			clean(lines = 2)
 
-	def has_writting_perm(self):
-		'''
-			Checks writting permissions
-		'''
 		while(not os.access('src/client/receiptmanager/receipts', os.W_OK)):
-			print( colorize("I have no permissions to save your receipt, please give write permissions at src/client/receiptmanager/receipts", 'red') )
+			print( colorize("I have no WRITE permissions, please allow WRITE permissions at src/client/receiptmanager/receipts", 'red') )
 			input("Press any key to try again...")
 			clean(lines = 2)
