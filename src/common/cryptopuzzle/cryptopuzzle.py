4b1df366060035932552be53928f9b848d8971bd
asergio <asergio@ua.pt>
2019-01-24 12:54:03 +0000

    Adds Improvements on CryptoPuzzle

--- src/common/cryptopuzzle/cryptopuzzle.py
+++ src/common/cryptopuzzle/cryptopuzzle.py
@@ -28,54 +28,62 @@ class CryptoPuzzle:
     # Saves puzzles sent to clients
     sent_puzzles = ExpiringDict()
 
-    def create_puzzle(self, public_key):
+    def create_puzzle(self, certificate):
         '''
             Creates puzzle to solve
 
             inputs:
-                public_key - public key of user that will solve the puzzle
+                certificate - certificate of user that will solve the puzzle
             output:
-                puzzle - hashed result of public_key xor random value
-                starts_with - beggining of random value used on hash function
+                puzzle - hashed result of hashed certificate xor random value
+                starts_with - beggining of random value used on hash function (0 - 70%)
                 ends_with - ending of random value used on hash function
 
             There are 2 bytes left between starts_with and ends_with that the
                 user needs to find out using brute force
         '''
-        solution = os.urandom( int(len(public_key) / 4) )
-        plain = self.string_xor(public_key[:int(len(public_key) / 4)], solution)
+        certificate_digest = SHA256.new(data=certificate).digest()
+        solution = os.urandom( len(certificate_digest) )
+        plain = self.string_xor(certificate_digest, solution)
         puzzle = SHA256.new(data=plain).hexdigest()
 
-        starts_with_index = randint(0, int( len(public_key) / 4 * random.uniform(0, 1) ) )
+        starts_with_index = randint(0, int( len(certificate_digest) * random.uniform(0, 0.7) ) )
         ends_with_index = starts_with_index + 2
 
         starts_with = solution[:starts_with_index]
         ends_with = solution[ends_with_index:]
 
-        self.sent_puzzles[public_key] = (puzzle, time.time() + 60)
+        self.sent_puzzles[certificate] = (puzzle, time.time() + 120)
 
         return puzzle, starts_with, ends_with
 
-    def validate_solution(self, public_key, solution):
+    def validate_solution(self, certificate, solution):
         '''
             Validates possible solution, also returns false if the puzzle is expired (60 seconds)
         '''
-        return self.sent_puzzles[public_key][0] == SHA256.new( data= self.string_xor(public_key, solution) ).hexdigest()
+        certificate_digest = SHA256.new(data=certificate).digest()
+        return self.sent_puzzles[certificate][0] == SHA256.new( data= self.string_xor(certificate_digest, solution) ).hexdigest() if self.sent_puzzles[certificate] else False
 
-    def solve_puzzle(self, puzzle, public_key, starts_with, ends_with):
+    def solve_puzzle(self, puzzle, certificate, starts_with, ends_with):
         '''
             Try to solve puzzle (user side)
         '''
-        attempt = self.build_attempt(starts_with, ends_with)
-        while (puzzle != SHA256.new( data= self.string_xor(public_key[:int(len(public_key) / 4)], attempt) ).hexdigest()):
-            attempt = self.build_attempt(starts_with, ends_with)
-        return attempt
+        certificate_digest = SHA256.new(data=certificate).digest()
+        sw = self.string_xor(certificate_digest[:len(starts_with)], starts_with)
+        ew = self.string_xor(certificate_digest[len(starts_with)+2:], ends_with)
+        cert = certificate_digest[len(starts_with):len(starts_with)+2]
 
-    def build_attempt(self, starts_with, ends_with):
+        attempt = self.build_attempt(sw, ew, cert)
+        while (puzzle != SHA256.new( data= attempt[0] ).hexdigest()):
+            attempt = self.build_attempt(sw, ew, cert)
+        return starts_with + attempt[1] + ends_with
+
+    def build_attempt(self, starts_with, ends_with, cert):
         '''
             Build an attempt of a solution
         '''
-        return starts_with + os.urandom(2) + ends_with
+        attempt = os.urandom(2)
+        return (starts_with + self.string_xor(cert, attempt) + ends_with, attempt)
 
     def string_xor(self, stringA, stringB):
         '''
